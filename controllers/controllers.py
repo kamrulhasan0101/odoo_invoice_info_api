c46425bd46e12fef69a94ebdcb69aeafd297e17e
Kamrul Hasan <kamrulhasan0101@gmail.com>
2021-10-27 18:10:43 +0600

    json response returned with get method

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 from odoo import http
+from odoo.http import Response
+from odoo.http import json
 
 
 class InvoiceInfoApi(http.Controller):
-    @http.route('/invoice-info-api/', auth='public')
+    @http.route('/invoice-info-api/', methods=['GET'], type='http', csrf=False, auth="public")
     def index(self, **kw):
         invoice = http.request.env['account.move'].search([('name', '=', 'INV/2021/10/0004')])
-        return str(invoice['amount_total'])
+        # return str(invoice['amount_total'])
+        return Response(json.dumps({"name": invoice['name'],
+                                    "amount_total": str(invoice['amount_total']),
+                                    "invoice_date": str(invoice['invoice_date']),
+                                    "invoice_date_due": str(invoice['invoice_date_due']),
+                                    "invoice_partner_display_name": invoice['invoice_partner_display_name'],
+                                    "create_date": str(invoice['create_date']),
+                                    }),
+                        content_type='application/json;charset=utf-8', status=200)
 
     # @http.route('/invoice_info_api/invoice_info_api/objects/', auth='public')
     # def list(self, **kw):
