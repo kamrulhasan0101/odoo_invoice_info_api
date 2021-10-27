597bf58e9fe0d0ebfb512629e77f7807ab83bc6e
Kamrul Hasan <kamrulhasan0101@gmail.com>
2021-10-27 19:11:11 +0600

    json response returned with get method

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -5,19 +5,21 @@ from odoo.http import json
 
 
 class InvoiceInfoApi(http.Controller):
-    @http.route('/invoice-info-api/', methods=['GET'], type='http', csrf=False, auth="public")
+    @http.route('/invoice-info-api/', methods=['POST'], type='http', csrf=False, auth="public")
     def index(self, **kw):
         invoice = http.request.env['account.move'].search([('name', '=', 'INV/2021/10/0004')])
-        # return str(invoice['amount_total'])
-        return Response(json.dumps({"name": invoice['name'],
-                                    "amount_total": str(invoice['amount_total']),
-                                    "invoice_date": str(invoice['invoice_date']),
-                                    "invoice_date_due": str(invoice['invoice_date_due']),
-                                    "invoice_partner_display_name": invoice['invoice_partner_display_name'],
-                                    "create_date": str(invoice['create_date']),
-                                    }),
-                        content_type='application/json;charset=utf-8', status=200)
-
+        if invoice:
+            return Response(json.dumps({"name": invoice['name'],
+                                        "amount_total": str(invoice['amount_total']),
+                                        "payment_state": invoice['payment_state'],
+                                        "invoice_date": str(invoice['invoice_date']),
+                                        "invoice_date_due": str(invoice['invoice_date_due']),
+                                        "invoice_partner_display_name": invoice['invoice_partner_display_name'],
+                                        "create_date": str(invoice['create_date']),
+                                        }),
+                            content_type='application/json;charset=utf-8', status=200)
+        else:
+            return "No Record Found"
     # @http.route('/invoice_info_api/invoice_info_api/objects/', auth='public')
     # def list(self, **kw):
     #     return http.request.render('invoice_info_api.listing', {
