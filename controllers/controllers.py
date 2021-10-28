a22a526e94e439f93552c2007e53801eb54c0bd1
Kamrul Hasan <kamrulhasan0101@gmail.com>
2021-10-28 18:41:07 +0600

    payment confirmation api on working

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -2,30 +2,39 @@
 from odoo import http
 from odoo.http import Response
 from odoo.http import json
+from odoo.http import request
 
 
 class InvoiceInfoApi(http.Controller):
-    @http.route('/invoice-info-api/', methods=['POST'], type='http', csrf=False, auth="public")
-    def index(self, **kw):
-        invoice = http.request.env['account.move'].search([('name', '=', 'INV/2021/10/0004')])
+    @http.route('/invoice-info-api/', methods=['GET'], type='http', csrf=False, auth="public")
+    def index(self, invoice_no):
+        invoice = http.request.env['account.move'].search([('name', '=', invoice_no)])
         if invoice:
-            return Response(json.dumps({"name": invoice['name'],
-                                        "amount_total": str(invoice['amount_total']),
-                                        "payment_state": invoice['payment_state'],
-                                        "invoice_date": str(invoice['invoice_date']),
-                                        "invoice_date_due": str(invoice['invoice_date_due']),
-                                        "invoice_partner_display_name": invoice['invoice_partner_display_name'],
-                                        "create_date": str(invoice['create_date']),
-                                        }),
+            data = {"status": True,
+                    "name": invoice['name'],
+                    "amount_total": str(invoice['amount_total']),
+                    "payment_state": invoice['payment_state'],
+                    "invoice_date": str(invoice['invoice_date']),
+                    "invoice_date_due": str(invoice['invoice_date_due']),
+                    "invoice_partner_display_name": invoice['invoice_partner_display_name'],
+                    "create_date": str(invoice['create_date']),
+                    }
+            return Response(json.dumps(data),
                             content_type='application/json;charset=utf-8', status=200)
         else:
-            return "No Record Found"
-    # @http.route('/invoice_info_api/invoice_info_api/objects/', auth='public')
-    # def list(self, **kw):
-    #     return http.request.render('invoice_info_api.listing', {
-    #         'root': '/invoice_info_api/invoice_info_api',
-    #         'objects': http.request.env['invoice_info_api.invoice_info_api'].search([]),
-    #     })
+            data = {"status": False, "message": "No Data Found"}
+            return Response(json.dumps(data),
+                            content_type='application/json;charset=utf-8', status=200)
+
+    @http.route('/invoice-info-api/payment-confirmation-with-status/', methods=['POST'], type='http', csrf=False, auth="public")
+    def payment_confirmation(self, **kw):
+        payment_status = kw['payment_status']
+        data = {"status": True,
+                "message": "Success",
+                "payment_status": payment_status,
+                }
+        return Response(json.dumps(data),
+                        content_type='application/json;charset=utf-8', status=200)
 
     # @http.route('/invoice_info_api/invoice_info_api/objects/<model("invoice_info_api.invoice_info_api"):obj>/', auth='public')
     # def object(self, obj, **kw):
