e171477c4b620aae2d4acf0428ce9d4294529a3b
Kamrul Hasan <kamrulhasan0101@gmail.com>
2021-11-07 18:21:10 +0600

    some thnig returned

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
+import logging
+import pprint
+import werkzeug
+from werkzeug import urls
+from odoo.addons.payment.models.payment_acquirer import ValidationError
+#imported from 1card module
+_logger = logging.getLogger(__name__)
+
 from odoo import http
 from odoo.http import Response
 from odoo.http import json
@@ -10,8 +18,7 @@ class InvoiceInfoApi(http.Controller):
     def index(self, invoice_no):
         invoice = http.request.env['account.move'].sudo().search([('name', '=', invoice_no)])
         if invoice:
-            data = {"status": True,
-                    "name": invoice['name'],
+            data = {"name": invoice['name'],
                     "amount_total": str(invoice['amount_total']),
                     "payment_state": invoice['payment_state'],
                     "invoice_date": str(invoice['invoice_date']),
@@ -19,25 +26,31 @@ class InvoiceInfoApi(http.Controller):
                     "invoice_partner_display_name": invoice['invoice_partner_display_name'],
                     "create_date": str(invoice['create_date']),
                     }
-            return Response(json.dumps(data),
+            response_data = {
+                "status": True,
+                "message": "success",
+                "data": data
+            }
+            return Response(json.dumps(response_data),
                             content_type='application/json;charset=utf-8', status=200)
         else:
-            data = {"status": False, "message": "No Data Found"}
-            return Response(json.dumps(data),
+            response_data = {"status": False, "message": "fail", "data": ''}
+            return Response(json.dumps(response_data),
                             content_type='application/json;charset=utf-8', status=200)
 
     @http.route('/invoice-info-api/payment-confirmation-with-status/', methods=['POST'], type='http', csrf=False, auth="public")
-    def payment_confirmation(self, **kw):
-        payment_status = kw['payment_status']
-        data = {"status": True,
-                "message": "Success",
-                "payment_status": payment_status,
+    def payment_confirmation(self, **post):
+        invoice_no = post['invoice_no']
+        paid_amount = post['paid_amount']
+
+        data = {"invoice_no": invoice_no,
+                "paid_amount": paid_amount,
                 }
-        return Response(json.dumps(data),
+        response_data = {
+            "status": True,
+            "message": "success",
+            "data": data
+        }
+        return Response(json.dumps(response_data),
                         content_type='application/json;charset=utf-8', status=200)
 
-    # @http.route('/invoice_info_api/invoice_info_api/objects/<model("invoice_info_api.invoice_info_api"):obj>/', auth='public')
-    # def object(self, obj, **kw):
-    #     return http.request.render('invoice_info_api.object', {
-    #         'object': obj
-    #     })
